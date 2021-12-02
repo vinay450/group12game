c3817aa1f7201b7c1c61e2c5dc7b5248def6aed4
Kyaneos1 <90323021+Kyaneos1@users.noreply.github.com>
2021-12-01 23:48:21 -0500

    Delete Phaser-Clicker-final directory

--- Phaser-Clicker-final/chrome-app-host.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('index.html', {
-      id:"mainWin",
-      frame: "none",
-      innerBounds: {width: 100, height: 100}
-  }, function(win) {
-    win.contentWindow.launchData = launchData;
-  });
-});
